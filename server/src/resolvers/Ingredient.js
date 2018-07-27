59f961c09635c9d7997063c2a2fab4f1d089c013
Nathanael Sherwood <Sherwood.Nathanael@gmail.com>
2018-07-27 10:43:06 -0600

    altered ingredient resolvers

--- server/src/resolvers/Ingredient.js
+++ server/src/resolvers/Ingredient.js
@@ -1,31 +1,31 @@
-const { getUserId } = require('../utils')
+// const { getUserId } = require('../utils')
 
 const ingredient = {
-  feed(parent, args, ctx, info) {
-    return ctx.db.query.ingredients({ where: { isPublished: true } }, info)
-  },
-
-  drafts(parent, args, ctx, info) {
-    const id = getUserId(ctx)
-
-    const where = {
-      isPublished: false,
-      author: {
-        id
-      }
-    }
-
-    return ctx.db.query.ingredients({ where }, info)
-  },
+  // feed(parent, args, ctx, info) {
+  //   return ctx.db.query.ingredients({ where: { isPublished: true } }, info)
+  // },
+  //
+  // drafts(parent, args, ctx, info) {
+  //   const id = getUserId(ctx)
+  //
+  //   const where = {
+  //     isPublished: false,
+  //     author: {
+  //       id
+  //     }
+  //   }
+  //
+  //   return ctx.db.query.ingredients({ where }, info)
+  // },
 
   ingredients(parent, { id }, ctx, info) {
     return ctx.db.query.ingredients({ where: { id } }, info)
   },
 
-  me(parent, args, ctx, info) {
-    const id = getUserId(ctx)
-    return ctx.db.query.user({ where: { id } }, info)
-  },
+  // me(parent, args, ctx, info) {
+  //   const id = getUserId(ctx)
+  //   return ctx.db.query.user({ where: { id } }, info)
+  // },
 }
 
 module.exports = { ingredient }
--- src/Ingredients/IngredientCreate.js
+++ src/Ingredients/IngredientCreate.js
@@ -2,6 +2,88 @@ import React, { Component } from 'react'
 import { Mutation } from "react-apollo";
 import gql from "graphql-tag";
 
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import Button from '@material-ui/core/Button';
+
+const styles = theme => ({
+    button: {
+      display: 'block',
+      marginTop: theme.spacing.unit * 2,
+    },
+    formControl: {
+      margin: theme.spacing.unit,
+      minWidth: 120,
+    },
+  });
+
+class ControlledOpenSelect extends React.Component {
+  state = {
+    ingredient: '',
+    open: false,
+  };
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
+  handleOpen = () => {
+    this.setState({ open: true });
+  };
+
+  render() {
+    const { classes } = this.props;
+
+  return (
+      <form autoComplete="off">
+        <Button className={classes.button} onClick={this.handleOpen}>
+          Open for Ingredient List
+        </Button>
+        <FormControl className={classes.formControl}>
+          <InputLabel htmlFor="demo-controlled-open-select">Ingredient</InputLabel>
+          <Select
+            open={this.state.open}
+            onClose={this.handleClose}
+            onOpen={this.handleOpen}
+            value={this.state.ingredient}
+            onChange={this.handleChange}
+            inputProps={{
+              name: 'ingredient',
+              id: 'demo-controlled-open-select',
+            }}
+          >
+            <MenuItem value="">
+            <em>None</em>
+            </MenuItem>
+            <MenuItem value="spice">Spice</MenuItem>
+            <MenuItem value="meat">Meat</MenuItem>
+            <MenuItem value="Vegetable">Vegetable</MenuItem>
+            <MenuItem value="fruit">Fruit</MenuItem>
+            <MenuItem value="grains">Grains</MenuItem>
+            <MenuItem value="dairy">Dairy</MenuItem>
+            <MenuItem value="beans">Beans</MenuItem>
+            <MenuItem value="pasta">Pasta</MenuItem>
+           </Select>
+         </FormControl>
+      </form>
+    );
+  }
+}
+
+ControlledOpenSelect.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export const CustomSelect = withStyles(styles)(ControlledOpenSelect);
+
 export default class extends Component {
   state = {
     name: "",
@@ -20,6 +102,7 @@ export default class extends Component {
   `;
 
   render () {
+
     return (
       <Mutation mutation={this.createIngredientMutation}>
         {(createIngred, {data}) => (
@@ -33,9 +116,9 @@ export default class extends Component {
               }})
             window.location.reload(true)
             }}>
-              <imput type="text" placeholder="name" required onChange {event => this.setState({ name : event.target.value})}/>
-              <imput type="text" placeholder="kind" required onChange {event => this.setState({ kind : event.target.value})}/>
-              <imput type="text" placeholder="substitute" required onChange {event => this.setState({ substitute : event.target.value})}/>
+              <imput type="text" placeholder="name" required onChange={event => this.setState({ name : event.target.value})}/>
+              <CustomSelect/>
+              <imput type="text" placeholder="substitute" required onChange={event => this.setState({ substitute : event.target.value})}/>
               <input type="submit" />
             </form>
           </div>
